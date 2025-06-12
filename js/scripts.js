fa1afa50cb905d44df18de5d06dbc902a1ebfafb
Vinicius Matos <viniciusmatosfelix2022@gmail.com>
2025-06-12 13:53:48 -0300

    feat - adição dos scroll link no header e mudanças no visual do button hamburguer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    /* Scroll into view */
+    const inicio = document.getElementById('inicio-scrol');
+    const inicioin = document.getElementById('introduction-in');
+
+    const sobre = document.getElementById('sobre-scrol');
+    const sobrein = document.getElementById('sobre-in');
+
+    const personagem = document.getElementById('personagens-scrol')
+    const personagemin = document.getElementById('personagem-in')
+
+    const galeria = document.getElementById('galeria-scrol');
+    const galeriain = document.getElementById('gallery-in');
+
+    /* Button Hamburguer */ 
     const icon = document.getElementById('ham-btn');
     const links = document.getElementById('links-chest');
 
@@ -7,7 +22,26 @@ document.addEventListener("DOMContentLoaded", () => {
         if (links.style.display == "flex") {
             links.style.display = "none";
         } else {
-            links.style.display = "flex"
+            links.style.display = "block"
         };
     });
+
+    /* Scroll into view código */
+
+    inicio.addEventListener("click", function() {
+        inicioin.scrollIntoView({behavior: "smooth"});
+    });
+
+    sobre.addEventListener("click", function(){
+        sobrein.scrollIntoView({behavior: "smooth"});
+    });
+
+    personagem.addEventListener("click", function() {
+        personagemin.scrollIntoView({behavior: "smooth"});
+    })
+
+    galeria.addEventListener("click", function() {
+        galeriain.scrollIntoView({behavior: "smooth"});
+    });
+
 });

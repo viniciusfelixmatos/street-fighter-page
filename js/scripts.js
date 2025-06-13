ee4fb4ac63afbbb60da279fcd6d3ddfdd9b4b134
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-06-13 14:36:09 -0300

    fix - Aprimoramento no botão Hamburguer

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    /* Scroll into view */
+    // Scroll Elements
     const inicio = document.getElementById('inicio-scrol');
     const inicioin = document.getElementById('introduction-in');
 
     const sobre = document.getElementById('sobre-scrol');
     const sobrein = document.getElementById('sobre-in');
 
-    const personagem = document.getElementById('personagens-scrol')
-    const personagemin = document.getElementById('personagem-in')
+    const personagem = document.getElementById('personagem-scrol');
+    const personagemin = document.getElementById('personagem-in');
 
     const galeria = document.getElementById('galeria-scrol');
     const galeriain = document.getElementById('gallery-in');
 
-    /* Button Hamburguer */ 
+    // Botão Hamburguer
     const icon = document.getElementById('ham-btn');
-    const links = document.getElementById('links-chest');
+    const navLinks = document.getElementById("nav-links");
 
+    if (icon && navLinks) {
+        icon.addEventListener("click", function () {
+            icon.classList.toggle('active');
+            navLinks.classList.toggle('active');
+        });
+    }
 
-    icon.addEventListener("click", function() {
-        if (links.style.display == "flex") {
-            links.style.display = "none";
-        } else {
-            links.style.display = "flex"
-        };
-    });
+    // Scroll into view com verificações
+    if (inicio && inicioin) {
+        inicio.addEventListener("click", function() {
+            inicioin.scrollIntoView({ behavior: "smooth" });
+        });
+    }
 
-    /* Scroll into view código */
+    if (sobre && sobrein) {
+        sobre.addEventListener("click", function() {
+            sobrein.scrollIntoView({ behavior: "smooth" });
+        });
+    }
 
-    inicio.addEventListener("click", function() {
-        inicioin.scrollIntoView({behavior: "smooth"});
-    });
+    if (personagem && personagemin) {
+        personagem.addEventListener("click", function() {
+            personagemin.scrollIntoView({ behavior: "smooth" });
+        });
+    }
 
-    sobre.addEventListener("click", function(){
-        sobrein.scrollIntoView({behavior: "smooth"});
-    });
-
-    personagem.addEventListener("click", function() {
-        personagemin.scrollIntoView({behavior: "smooth"});
-    })
-
-    galeria.addEventListener("click", function() {
-        galeriain.scrollIntoView({behavior: "smooth"});
-    });
-
+    if (galeria && galeriain) {
+        galeria.addEventListener("click", function() {
+            galeriain.scrollIntoView({ behavior: "smooth" });
+        });
+    }
 });
